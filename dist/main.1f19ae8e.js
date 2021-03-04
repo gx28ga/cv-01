33dd69e70acf3a522a35a02439db8eed730b3abb
guanxin <373103730@qq.com>
2021-03-04 22:11:19 +0800

    fix source url

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -195,7 +195,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58152" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51046" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -371,4 +371,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../usr/local/lib/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=dist/main.1f19ae8e.js.map
